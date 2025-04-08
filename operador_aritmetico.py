312876e609c0ce4281480295f8ab0c61c5df6061
Adan909 <jorgeot@uamv.edu.ni>
2025-04-07 19:35:43 -0600

    cree el repositorio s4

---
+++ operador_aritmetico.py
@@ -0,0 +1,5 @@
+#suma de dos numeros
+numero1 = int(input("Dime un numero: "))
+numero2 = int(input("Dime otro numero: "))
+suma = numero1 + numero2
+print(f"La suma de \n  {numero1:>6,} \n+ {numero2:>6,} \n= {suma:>6,}")
