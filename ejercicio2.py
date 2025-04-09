084f90aacf67a01dada3f431fb1ddd1450808d46
Fjzav-pro <fjzavalam@uamv.edu.ni>
2025-04-09 14:40:22 -0600

    calcular salario de trabajador

---
+++ ejercicio2.py
@@ -0,0 +1,4 @@
+hortrab = float(input("Dime las horas trabajadas: "))
+prechor = float(input("Cuanto ganas por hora?: "))
+sueld_bruto = hortrab * prechor
+print(f"Tu salario total es de: \n{sueld_bruto:,.2f}" )
